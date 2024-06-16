a2e7ba3e6781339038ca36f24798d7dff4130a38
BOPPIDIGOUTHAM <163089575+BOPPIDIGOUTHAM@users.noreply.github.com>
2024-06-16 17:18:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
-// import ButtonComponent from "./components/button/button.js";
-// import { Heading1,
-//          Heading2,
-//          Heading3,
-//          Heading4,
-//          Heading5,
-//          Heading6
-//         }from "./components/headings/headings.js";
+import ButtonComponent from "./components/button/button.js";
+import { Heading1,
+         Heading2,
+         Heading3,
+         Heading4,
+         Heading5,
+         Heading6
+        }from "./components/headings/headings.js";
 
 
-// import{ListParentOrdered,ListParentUnordered} from "./components/list/list.js"
+import{ListParentOrdered,ListParentUnordered} from "./components/list/list.js"
 
-// import  ImageComponent  from "./components/image/image.js";
+import  ImageComponent  from "./components/image/image.js";
 
-// import Card from "./components/card/card"
+import Card from "./components/card/card"
 import TableCreation  from "./components/table/table.js";
 
  const App=()=>{
    return(
    <div class="app">
-       {/* <ImageComponent/>
-//       <h2>hello goutham</h2>
-//       <ButtonComponent></ButtonComponent>
-//       <ImageComponent/>
-//       <h2>hello goutham</h2>
-//       <ButtonComponent></ButtonComponent>
-//       <ButtonComponent></ButtonComponent>
-//       <h2>hello react </h2>
+       <ImageComponent/>
+      <h2>hello goutham</h2>
+      <ButtonComponent></ButtonComponent>
+      <ImageComponent/>
+      <h2>hello goutham</h2>
+      <ButtonComponent></ButtonComponent>
+      <ButtonComponent></ButtonComponent>
+      <h2>hello react </h2>
 
-//       <ButtonComponent></ButtonComponent>
-//       <ButtonComponent></ButtonComponent>
-//        <Heading1></Heading1>
-//        <Heading2></Heading2>
-//        <Heading3></Heading3>
-//        <Heading4></Heading4>
-//        <Heading5></Heading5>
-//        <Heading6></Heading6>
+      <ButtonComponent></ButtonComponent>
+      <ButtonComponent></ButtonComponent>
+       <Heading1></Heading1>
+       <Heading2></Heading2>
+       <Heading3></Heading3>
+       <Heading4></Heading4>
+       <Heading5></Heading5>
+       <Heading6></Heading6>
        
 
-//        <ListParentOrdered/>
-//        <ListParentUnordered/>  */}
-        {/* <Card></Card>
-//         <Card></Card>
-//         <Card></Card> */}
+       <ListParentOrder
+        <ListParentUnordered/>  
+         <Card></Card>
+         <Card></Card>
+         <Card></Card> 
 
 
 
