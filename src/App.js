67c9caa5bb47537c336bfacc41d92b8701621a91
goutham boppidi <gouthamboppidi@gmail.com>
2024-08-01 22:26:29 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Weather from './components/weather/weather';
+// import Weather from './components/weather/weather';
+// import RecipeData from './components/recipes/recipesData';
+// import RecipeIngredient from './components/recipes/recipesIngredients';
+import Routing from './components/recipes/routing';
 
 function App() {
   return (
     <div>
-      <Weather />
-    </div>
+      {/* <Weather /> */}
+      {/* <RecipeData/> */}
+      {/* <RecipeIngredient/> */}
+      <Routing/>
+         </div>
   );
 }
 
